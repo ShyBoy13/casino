b9be4dbcc0f6b414fe1220acdc625051027bb577
Roman Rios <romanrs1301@gmail.com>
2024-07-03 17:29:38 -0700

    Ahora se puede cambiar los porcentajes por defecto y siempre se mantienen hasta un 100% sumando o restando al siguiente input lo que se quito o agrego del principal

--- public/js/calculadora-fichas.js
+++ public/js/calculadora-fichas.js
@@ -1,7 +1,7 @@
 const fichas_valores = [100, 50, 30, 20, 10]
-const fichas_porcentajes = [0.240, 0.25, 0.25, 0.20, 0.110]
+const fichas_porcentajes = [0.240, 0.25, 0.20, 0.20, 0.110]
 
-const calcularFichas = (cantidad) => {
+const calcularFichas = (cantidad, porcentajes) => {
   let valorEnFichas = parseFloat(cantidad) * 100;
   let cantidadFichasArr = []
   let valoresFichasCentavosArr = []
@@ -10,10 +10,10 @@ const calcularFichas = (cantidad) => {
   try {
     console.log((valorEnFichas * 100) % 10)
     if ( (valorEnFichas * 100) % 10 !== 0) throw new Error('El dinero debe de poder dividirse entre 10 centavos');
-    fichas_porcentajes.forEach((porcentaje, itr) => {
+    porcentajes.forEach((porcentaje, itr) => {
       let valorEnCentavos = (porcentaje * valorEnFichas)
-      let cantidadDeFicha = Math.floor(valorEnCentavos / fichas_valores[itr])
-      totalDeFichas += cantidadDeFicha
+      let cantidadDeFicha = Math.round(valorEnCentavos / fichas_valores[itr])
+     totalDeFichas += cantidadDeFicha
       totalDeCentavos += fichas_valores[itr] * cantidadDeFicha
       valoresFichasCentavosArr.push(fichas_valores[itr] * cantidadDeFicha)
       cantidadFichasArr.push(cantidadDeFicha)
@@ -31,14 +31,40 @@ const calcularFichas = (cantidad) => {
 }
 
 function main() {
+  let inputPorcentajes = Array.from(document.querySelectorAll('input[id^="porcentaje"]'))
+  inputPorcentajes.forEach((porcentajeElem, itr) => {
+    porcentajeElem.value = fichas_porcentajes[itr]
+  })
+  let porcentajesEstado = inputPorcentajes.map(porcentajeElem => parseFloat(porcentajeElem.value))
+  document.querySelector('#calculadora-elem').addEventListener('change', (e) => {
+    let posicionInput = inputPorcentajes.indexOf(e.target)
+    console.log(e.target.id.startsWith('porcentaje'), e.target.tagName, posicionInput+1)
+    if (e.target.id.startsWith('porcentaje') && e.target.tagName === 'INPUT') {
+      let siguientePorcentajeElem
+      if (inputPorcentajes.length === posicionInput+1)  siguientePorcentajeElem = inputPorcentajes[0]
+      else  siguientePorcentajeElem = e.target.parentElement.nextElementSibling.querySelector('input[id^="porcentaje"]')
+
+      console.log(porcentajesEstado[posicionInput], parseFloat(e.target.value))
+      siguientePorcentajeElem.value = parseFloat(siguientePorcentajeElem.value) + (porcentajesEstado[posicionInput] - parseFloat(e.target.value))
+      porcentajesEstado[posicionInput] =  e.target.value
+    }
+  })
   document.querySelector('#calculadora-elem').addEventListener('click', (e) => {
+    //console.log(e.currentTarget.querySelectorAll('input[id^="porcentaje"]'))
     let monto = e.currentTarget.querySelector('#cantidad-dinero').value
     if (e.target.id === 'calcular-btn' && monto !== '') {
-      let calculosFichas = calcularFichas(monto)
-      console.log(calculosFichas)
+      
+      let calculosFichas = calcularFichas(monto, porcentajesEstado.map(porcentajeElem => porcentajeElem.value))
+
+      console.log(fichas_porcentajes
+        .map((porcentaje, itr) => parseFloat(porcentajesEstado[itr].value))
+        .reduce((porcentajeTotal, porcentajeActual) => porcentajeTotal + porcentajeActual)
+      )
+
+      //console.log(calculosFichas)
       fichas_valores.forEach((ficha, itr) => {
-        console.log(e.currentTarget.querySelector(`#valor-${String(ficha)} > .calculo-fichas`))
-        let calculoFichasElem = e.currentTarget.querySelector(`#valor-${String(ficha)} > .calculo-fichas`)
+        //console.log(e.currentTarget.querySelector(`#valor-${String(ficha)} .calculo-fichas`))
+        let calculoFichasElem = e.currentTarget.querySelector(`#valor-${String(ficha)} .calculo-fichas`)
         calculoFichasElem.innerHTML = ''
         e.currentTarget.querySelector('#total-fichas').innerHTML = 'Total de fichas: '
         e.currentTarget.querySelector('#total-centavos').innerHTML = 'Centavos Totales: '
